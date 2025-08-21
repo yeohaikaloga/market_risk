8a6835a8a3f3630129efeb7e0c66f72a653df4ba
yeohaikal <45327666+yeohaikal@users.noreply.github.com>
2025-08-21 10:57:42 +0800

    created derivatives position loader and updated cotton var workflow

---
+++ quality_checker/cotton_var_checker.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from utils.contract_utils import custom_monthly_contract_sort_key
+
+def check_derivatives_positions(df):
+    check_deriv_pos_df = df[df['total_active_lots'] != 0]
+    check_deriv_pos_df = check_deriv_pos_df[
+        (~check_deriv_pos_df['portfolio'].str.contains('MILL OPTNS'))  # filter out MILL OPTNS from UNIT_SD
+        & (~check_deriv_pos_df['books'].isin(
+            ['ADMIN', 'POOL', 'NON OIL']))]  # filter out ADMIN, POOL, NON OIL from BOOKS
+    grouped_df = (check_deriv_pos_df.groupby(['region', 'derivative_type']).sum(numeric_only=True).
+                  reset_index().sort_values(by=['region', 'derivative_type']))
+    print(grouped_df)
+
+    pivot_df = pd.pivot_table(check_deriv_pos_df,
+                              index=['unit', 'derivative_type'],
+                              columns=['underlying_bbg_ticker'],
+                              values='total_active_lots',
+                              aggfunc='sum',
+                              fill_value=0).reset_index()  # fill missing combinations with 0
+    index_cols = ['unit', 'derivative_type']
+    cols_to_sort = [col for col in pivot_df.columns if col not in index_cols]
+    sorted_cols = sorted(cols_to_sort, key=custom_monthly_contract_sort_key)
+    sorted_columns = index_cols + sorted_cols
+    pivot_df = pivot_df[sorted_columns]
+    print(pivot_df)
+    pivot_df.to_csv('pivot_check.csv')
